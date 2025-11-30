268470a54b56924b0552d105c8b93b9c6b7aeaa8
Fernando Jaime <Ferjaime11@hotmail.com>
2025-11-30 18:03:19 -0300

    configuracion publish

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/power-land/",
   server: {
     host: "::",
     port: 8080,
